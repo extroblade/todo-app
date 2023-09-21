2d2d35256e6ad1b4529cc0d641d8e746ff4cc617
extroblade <mr.ovs0ford@gmail.com>
2023-09-21 16:11:45 +0400

    fix: app margins

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <div
       className={
-        'w-full md:max-w-4xl m-0 md:m-10 bg-sky-200 px-6 pb-12 pt-8 md:rounded-lg flex flex-col items-center justify-center'
+        'w-full md:max-w-4xl m-0 md:m-auto bg-sky-200 px-6 pb-12 pt-8 md:rounded-lg flex flex-col items-center justify-center'
       }
     >
       <h1 className="scroll-m-20 mb-5 text-4xl font-extrabold tracking-wide lg:text-5xl">ToDos</h1>
